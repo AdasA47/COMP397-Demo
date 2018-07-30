4ed10294ecc016ad5f2b1b530e07e19ffcdf5e07
Adithya Dasarapu <adithyadasarapu@gmail.com>
2018-07-30 01:35:58 -0400

    added typescript support

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,29 +1,37 @@
-(function () {
-    var canvas = document.getElementById("canvas");
-    var stage;
-    var helloLabel;
-    function Init() {
+(function(){
+    
+    let canvas = document.getElementById("canvas");
+    let stage: createjs.Stage;
+    let helloLabel: createjs.Text;
+
+    function Init(){
         console.log("Initialization started...");
         Start();
     }
-    function Start() {
+
+    function Start(){
         console.log("Starting Application...");
+
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60;
-        createjs.Ticker.on("tick", Update);
+        createjs.Ticker.on("tick",Update);
         Main();
     }
-    function Update() {
-        helloLabel.rotation += 1;
+    
+    function Update(){
+        helloLabel.rotation +=1;
         stage.update();
+
     }
-    function Main() {
+
+    function Main(){
         console.log("Game Started...");
+
         helloLabel = new createjs.Text("Hello, World!", "40px Consolas", "#000000");
         helloLabel.x = 100;
         helloLabel.y = 100;
         stage.addChild(helloLabel);
     }
+
     window.onload = Init;
 })();
-//# sourceMappingURL=game.js.map
